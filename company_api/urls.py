5b38bc0129a907269fd302ca145ab4b45d842221
Qiuyang Nie <qyang.nie@gmail.com>
2021-06-28 09:09:19 +0100

    Update name args in path

--- company_api/urls.py
+++ company_api/urls.py
@@ -5,6 +5,6 @@ app_name = 'company_api'
 
 urlpatterns = [
     path('companies/<str:pk>/quote', CompanyQuote.as_view(), name='company-quote'),
-    path('companies/<str:pk>', CompanyProfileDetail.as_view(), name='companyprofiledetail'),
-    path('companies', CompanyProfileList.as_view(), name='companyprofilelist')
+    path('companies/<str:pk>', CompanyProfileDetail.as_view(), name='company-profile'),
+    path('companies', CompanyProfileList.as_view(), name='company-profile-list')
 ]
