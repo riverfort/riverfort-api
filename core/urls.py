3f4e8629d8185ff824b0d8baf89b5e77a12b537d
Qiuyang Nie <qyang.nie@gmail.com>
2021-07-05 18:15:48 +0100

    Update url.py

--- core/urls.py
+++ core/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('company.urls', namespace='company')),
     path('api/', include('company_api.urls', namespace='company_api')),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
