d2d4c8efc093e529acdc3ad07f29bf022365c25d
Qiuyang Nie <qyang.nie@gmail.com>
2021-07-12 15:51:13 +0100

    Add v1 to company_api urls

--- company_api/urls.py
+++ company_api/urls.py
@@ -8,18 +8,18 @@ from .views import CompanyProfileList,  CompanyTradingList, CompanyAdtvList, \
 app_name = 'company_api'
 
 urlpatterns = [
-    path('companies/<str:pk>/trading/quote', CompanyTradingQuote.as_view(), name='company-trading-quote'),
-    path('companies/<str:pk>/adtv/quote', CompanyAdtvQuote.as_view(), name='company-adtv-quote'),
-    path('companies/<str:pk>/adtv', CompanyAdtvList.as_view(), name='company-adtv-list'),
-    path('companies/<str:pk>/trading', CompanyTradingList.as_view(), name='company-trading-list'),
-    path('companies/<str:pk>/quote', CompanyQuote.as_view(), name='company-quote'),
-    path('companies/<str:pk>', CompanyProfile.as_view(), name='company-profile'),
-    path('companies', CompanyProfileList.as_view(), name='company-profile-list'),
+    path('v1/companies/<str:pk>/trading/quote', CompanyTradingQuote.as_view(), name='company-trading-quote'),
+    path('v1/companies/<str:pk>/adtv/quote', CompanyAdtvQuote.as_view(), name='company-adtv-quote'),
+    path('v1/companies/<str:pk>/adtv', CompanyAdtvList.as_view(), name='company-adtv-list'),
+    path('v1/companies/<str:pk>/trading', CompanyTradingList.as_view(), name='company-trading-list'),
+    path('v1/companies/<str:pk>/quote', CompanyQuote.as_view(), name='company-quote'),
+    path('v1/companies/<str:pk>', CompanyProfile.as_view(), name='company-profile'),
+    path('v1/companies', CompanyProfileList.as_view(), name='company-profile-list'),
 
-    path('company/list', CompanyList.as_view(), name='company-list-create'),
-    path('company/list/fmp', FmpDataList.as_view(), name='fmp-list-create'),
-    path('company/list/iex', IexDataList.as_view(), name='iex-list-create'),
-    path("company/<str:ticker>/quote", company_quote_full, name="company-quote-full"),
+    path('v1/company/list', CompanyList.as_view(), name='company-list-create'),
+    path('v1/company/list/fmp', FmpDataList.as_view(), name='fmp-list-create'),
+    path('v1/company/list/iex', IexDataList.as_view(), name='iex-list-create'),
+    path("v1/company/<str:ticker>/quote", company_quote_full, name="company-quote-full"),
 
-    path('add-on', add_company, name="add-on"),
+    path('v1/add-on', add_company, name="add-on"),
 ]
