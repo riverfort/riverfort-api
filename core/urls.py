ce015afcaff2591ec9f2a68d3e97cc7fc830ba89
Qiuyang Nie <qyang.nie@gmail.com>
2021-07-19 17:52:46 +0100

    Add documentation

--- core/urls.py
+++ core/urls.py
@@ -3,11 +3,13 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from rest_framework.schemas import get_schema_view
+from rest_framework.documentation import include_docs_urls
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('company.urls', namespace='company')),
     path('api/', include('company_api.urls', namespace='company_api')),
+    path('docs', include_docs_urls(title='RiverFort REST API')),
     path('schema', get_schema_view(
         title="RiverFort REST API",
         description="Equity API",
