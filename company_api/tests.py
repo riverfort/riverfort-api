7ca260734f812478e38ca8d81c8b145246ecf256
Qiuyang Nie <qyang.nie@gmail.com>
2021-07-20 10:50:53 +0100

    Update kwargs

--- company_api/tests.py
+++ company_api/tests.py
@@ -23,22 +23,22 @@ class CompanyProfileTests(APITestCase):
         self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
 
     def test_view_company_trading_list(self):
-        url = reverse('company_api:company-trading-list', kwargs={'pk': 'non-existent-company'})
+        url = reverse('company_api:company-trading-list', kwargs={'pk': 'testing-company-ticker'})
         response = self.client.get(url, format='json')
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
     def test_view_company_adtv_list(self):
-        url = reverse('company_api:company-adtv-list', kwargs={'pk': 'non-existent-company'})
+        url = reverse('company_api:company-adtv-list', kwargs={'pk': 'testing-company-ticker'})
         response = self.client.get(url, format='json')
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
     def test_view_company_adtv_quote(self):
-        url = reverse('company_api:company-adtv-quote', kwargs={'pk': 'non-existent-company'})
+        url = reverse('company_api:company-adtv-quote', kwargs={'pk': 'testing-company-ticker'})
         response = self.client.get(url, format='json')
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
     def test_view_company_trading_quote(self):
-        url = reverse('company_api:company-trading-quote', kwargs={'pk': 'non-existent-company'})
+        url = reverse('company_api:company-trading-quote', kwargs={'pk': 'testing-company-ticker'})
         response = self.client.get(url, format='json')
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
@@ -58,15 +58,11 @@ class CompanyProfileTests(APITestCase):
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
     def test_view_company_quote_full(self):
-        url = reverse('company_api:company-quote-full', kwargs={'ticker': 'non-existent-company'})
+        url = reverse('company_api:company-quote-full', kwargs={'ticker': 'testing-company-ticker'})
         response = self.client.get(url, format='json')
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
     def test_create_add_on(self):
         url = reverse('company_api:add-on')
-        # data = {
-        #     "company_ticker": "testing_company_ticker",
-        #     "company_name": "testing_company_name"
-        # }
         response = self.client.get(url, format='json')
         self.assertEqual(response.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
