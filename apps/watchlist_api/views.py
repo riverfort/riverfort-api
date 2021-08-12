bd4842578b99d60911a6a7731f0e04dc3fe091d2
Qiuyang Nie <qyang.nie@gmail.com>
2021-08-12 11:59:42 +0100

    Add WatchlistDetail

--- apps/watchlist_api/views.py
+++ apps/watchlist_api/views.py
@@ -1,4 +1,5 @@
 from rest_framework import generics
+from rest_framework.response import Response
 from apps.watchlist.models import UserDevice, Company, Watchlist
 from .serializers import UserDeviceSerializer, CompanySerializer, WatchlistSerializer
 
@@ -21,3 +22,30 @@ class CompanyList(generics.ListCreateAPIView):
 class WatchlistList(generics.ListCreateAPIView):
     queryset = Watchlist.objects.all()
     serializer_class = WatchlistSerializer
+
+
+class MultipleFieldLookupMixin:
+    """
+    Apply this mixin to any view or viewset to get multiple field filtering
+    based on a `lookup_fields` attribute, instead of the default single field filtering.
+
+    Source: https://www.django-rest-framework.org/api-guide/generic-views/#creating-custom-mixins
+    Modified to not error out for not providing all fields in the url.
+    """
+    def get_object(self):
+        from django.shortcuts import get_object_or_404
+        queryset = self.get_queryset()             # Get the base queryset
+        queryset = self.filter_queryset(queryset)  # Apply any filter backends
+        filter = {}
+        for field in self.lookup_fields:
+            if self.kwargs.get(field):  # Ignore empty fields.
+                filter[field] = self.kwargs[field]
+        obj = get_object_or_404(queryset, **filter)  # Lookup the object
+        self.check_object_permissions(self.request, obj)
+        return obj
+
+
+class WatchlistDetail(MultipleFieldLookupMixin, generics.RetrieveUpdateDestroyAPIView):
+    queryset = Watchlist.objects.all()
+    serializer_class = WatchlistSerializer
+    lookup_fields = ['device', 'company_ticker']
