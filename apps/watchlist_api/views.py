074b821a5eb1a30e8158934bc9d31a7a5d040d83
Qiuyang Nie <qyang.nie@gmail.com>
2021-08-12 12:00:26 +0100

    Remove import

--- apps/watchlist_api/views.py
+++ apps/watchlist_api/views.py
@@ -1,5 +1,4 @@
 from rest_framework import generics
-from rest_framework.response import Response
 from apps.watchlist.models import UserDevice, Company, Watchlist
 from .serializers import UserDeviceSerializer, CompanySerializer, WatchlistSerializer
 
