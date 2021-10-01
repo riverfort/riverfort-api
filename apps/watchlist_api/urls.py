797108ea80ec65ce65cbb1836f94426f712b83b7
Qiuyang Nie <qyang.nie@gmail.com>
2021-10-01 11:21:52 +0100

    Update path

--- apps/watchlist_api/urls.py
+++ apps/watchlist_api/urls.py
@@ -6,7 +6,7 @@ app_name = 'notification_api'
 urlpatterns = [
     path('v1', views.WatchlistList.as_view(), name='watchlist'),
     path(
-      'v1/device/<str:device_token>/company/<str:company_ticker>',
+      'v1/device-token/<str:device_token>/company-symbol/<str:company_symbol>',
       views.WatchlistDetail.as_view(),
       name='watchlist-detail'),
     path('v1/device-tokens', views.DeviceTokensList.as_view(), name='device-token-list'),
